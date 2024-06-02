7f6d66f61646664e0a43a8b4d8ea8996abdf8868
sdasd <vpaira@gmail.com>
2024-06-02 22:47:19 +0530

    INitial

---
+++ views/index.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+
+def IndexView(page, params):
+    def CreateAppBar():
+        app_bar = ft.AppBar(
+            leading=ft.Icon(ft.icons.PALETTE, color="#e45678"),
+            leading_width=40,
+            title=ft.Text("Flet Template"),
+            center_title=False,
+            bgcolor=ft.colors.SURFACE_VARIANT,
+            actions=[
+                ft.IconButton(ft.icons.RESTART_ALT, on_click=restart_clicked),
+                ft.IconButton(ft.icons.FILTER_3),
+
+            ],
+        )
+        return app_bar
+
+    def restart_clicked(e):
+        print("restart")
+
+    def btn_question1_clicked(e):
+        page.go("/question/1")
+
+    def btn_question2_clicked(e):
+        page.go("/question/2")
+
+    def btn_simple_clicked(e):
+        page.go("/simple_view")
+
+    txt = ft.Text("Welocome to the Flet Template")
+    col_right = ft.Row(controls=[txt], alignment=ft.MainAxisAlignment.END)
+    btn_question1 = ft.ElevatedButton("Question1", on_click=btn_question1_clicked)
+    btn_question2 = ft.ElevatedButton("Question2", on_click=btn_question2_clicked)
+    btn_simple = ft.ElevatedButton("Simple View", on_click=btn_simple_clicked)
+    img_1 = ft.Image(src="images/m1.jpg", width=300)
+    appbar = CreateAppBar()
+
+    page.views.append(ft.View(
+        "/",
+        [appbar, col_right, btn_question1, btn_question2, btn_simple, img_1],
+
+    )
+    )
+    page.update()
---
+++ views/question.py
@@ -0,0 +1,36 @@
+import flet as ft
+import random
+
+
+def QuestionView(page, params):
+    def letter_clicked(e):
+        con_1.content.value = chr(ord(con_1.content.value) + 1)
+        con_1.left = random.randrange(0, 200)
+        con_1.top = random.randrange(0, 200)
+        con_1.update()
+
+    question_no = params["id"]
+    txt = ft.Text(f"Question {question_no}")
+    btn = ft.ElevatedButton("Home", on_click=lambda _: page.go("/"))
+    appbar = ft.AppBar(
+        title=ft.Text("Question"),
+        bgcolor=ft.colors.SURFACE_VARIANT
+    )
+
+    txt_letter1 = ft.Text("A", text_align=ft.TextAlign.CENTER, size=22)
+
+    con_1 = ft.Container(
+        content=txt_letter1,
+        border=ft.border.all(2, ft.colors.AMBER_800),
+        width=35,
+        height=35,
+        bgcolor="#FFEDBAC2",
+        top=0,
+        left=0,
+        animate_position=300,
+        on_click=letter_clicked,
+
+    )
+    stk = ft.Stack(controls=[con_1])
+    page.views.append(ft.View("/question", [appbar, txt, btn, stk]))
+    page.update()
