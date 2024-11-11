0ecee9fcf943cb4f46075ff6c9ae26d544a748a7
imdanmoili <imdanmoili@outlook.com>
2024-11-11 16:08:43 +0530

    Change title

--- views/index.py
+++ views/index.py
@@ -5,7 +5,7 @@ def IndexView(page:ft.Page, params):
         app_bar = ft.AppBar(
             leading=ft.Image("images/csc_logo_100.png"),
             leading_width=40,
-            title=ft.Text("Flet Template"),
+            title=ft.Text("My Flet Template"),
             #center_title=False,
             bgcolor=ft.colors.SURFACE_VARIANT,
             actions=[
